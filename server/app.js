259281df79be57c8131a63067c5fd61668741a9a
lucasmodin <lumo0002@stud.kea.dk>
2025-12-09 00:12:02 +0100

    updated app.js with socket support

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 
 const PORT = Number(process.env.PORT) || 8080;
